ea1e48fb436796148d22de6c9e26616d7aa54ee6
theSalko <zlatko_lagumdzija@alumni.brown.edu>
2025-05-22 08:19:58 +0800

    added the countdown

--- scripts/countdown_script.js
+++ scripts/countdown_script.js
@@ -28,10 +28,10 @@ function startCountdown(targetDate, elementId) {
   
   // Set target dates
   // const discountEndDate = new Date("January 27, 2025 23:59:59 UTC -12").getTime();
-  const discountEndDate = Date.UTC(2025, 0, 28, 11, 59, 59);
+  const discountEndDate = Date.UTC(2025, 5, 4, 11, 59, 59);
 //   const discountEndDate = new Date(Date.UTC(2025, 0, 27, 23, 59, 59));
   // const applicationCloseDate = new Date("January 30, 2025 23:59:59 UTC -12").getTime()
-  const applicationCloseDate = Date.UTC(2025, 0, 31, 11, 59, 59); // January 31, 2025, 11:59:59 UTC (handles UTC-12)
+  const applicationCloseDate = Date.UTC(2025, 6, 22, 11, 59, 59); // January 31, 2025, 11:59:59 UTC (handles UTC-12)
   
   // Start the countdowns
   startCountdown(discountEndDate, "discountTimer");
