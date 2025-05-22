1da7d2aee9f98e093081b77bf72e71eeef463f57
theSalko <zlatko_lagumdzija@alumni.brown.edu>
2025-05-23 00:55:52 +0800

    Added the pdfs of the test and reference problems

--- scripts/countdown_script.js
+++ scripts/countdown_script.js
@@ -31,7 +31,7 @@ function startCountdown(targetDate, elementId) {
   const discountEndDate = Date.UTC(2025, 5, 4, 11, 59, 59);
 //   const discountEndDate = new Date(Date.UTC(2025, 0, 27, 23, 59, 59));
   // const applicationCloseDate = new Date("January 30, 2025 23:59:59 UTC -12").getTime()
-  const applicationCloseDate = Date.UTC(2025, 6, 22, 11, 59, 59); // January 31, 2025, 11:59:59 UTC (handles UTC-12)
+  const applicationCloseDate = Date.UTC(2025, 6, 22, 11, 59, 59); // July 22, 2025, 11:59:59 UTC (handles UTC-12)
   
   // Start the countdowns
   startCountdown(discountEndDate, "discountTimer");
